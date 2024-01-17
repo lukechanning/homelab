e9d2d61d6d5b6db030452f4698cad555d3f6be77
Luke Patrick <luke@routerchowder.com>
2024-01-17 08:15:08 -0600

    feat(infra): wire up services and ingresses for pihole via traefik

--- infra/modules/pihole/main.tf
+++ infra/modules/pihole/main.tf
@@ -5,37 +5,68 @@ resource "kubernetes_namespace" "pihole-cloud" {
 }
 
 # define the ingress for pihole DNS service
-# resource "kubernetes_manifest" "longhorn-networking" {
-#   manifest = {
-#     apiVersion = "traefik.containo.us/v1alpha1"
-#     kind       = "IngressRoute"
+resource "kubernetes_manifest" "pihole-networking" {
+  manifest = {
+    apiVersion = "traefik.containo.us/v1alpha1"
+    kind       = "IngressRouteUDP"
 
-#     metadata = {
-#       name      = "pihole-dns"
-#       namespace = "pihole-cloud"
-#     }
+    metadata = {
+      name      = "pihole-external-dns"
+      namespace = "pihole-cloud"
+    }
 
-#     spec = { 
-#       entryPoints = [
-#         "web"
-#       ]
+    spec = { 
+      entryPoints = [
+        "udp-dns"
+      ]
 
-#       routes = [
-#         {
-#           match = "Host(`longhorn`)"
-#           kind = "Rule"
-#           services = [
-#             {
-#               name = "longhorn-frontend-ui"
-#               namespace = "longhorn-system"
-#               port = 80
-#             }
-#           ]
-#         }
-#       ]
-#     }
-#   }
-# }
+      routes = [
+        {
+          services = [
+            {
+              name = "pihole-dns-udp"
+              namespace = "pihole-cloud"
+              port = 53 
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+
+# define the ingress for pihole web service
+resource "kubernetes_manifest" "pihole-web-networking" {
+  manifest = {
+    apiVersion = "traefik.containo.us/v1alpha1"
+    kind       = "IngressRoute"
+
+    metadata = {
+      name      = "pihole-external-web"
+      namespace = "pihole-cloud"
+    }
+
+    spec = { 
+      entryPoints = [
+        "web"
+      ]
+
+      routes = [
+        {
+          match = "Host(`pihole`)"
+          kind = "Rule"
+          services = [
+            {
+              name = "pihole-web"
+              namespace = "pihole-cloud"
+              port = 80 
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
 
 resource "helm_release" "pihole" {
   name       = "pihole"
